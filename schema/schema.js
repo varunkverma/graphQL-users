4f45e6f611120598839d7ad056c21d7883a2a7e2
varunkverma <varunkverma1996@gmail.com>
2020-06-02 00:20:34 +0530

    implemented json-server to mimic another server, to which graphQL can make request to. implemented associative nature between user and company

--- schema/schema.js
+++ schema/schema.js
@@ -1,11 +1,15 @@
 const graphql = require("graphql");
 const { GraphQLObjectType, GraphQLInt, GraphQLString, GraphQLSchema } = graphql;
-const _ = require("lodash");
+const axios = require("axios");
 
-const users = [
-  { id: "23", firstName: "Bill", age: 20 },
-  { id: "47", firstName: "Samantha", age: 21 },
-];
+const CompanyType = new GraphQLObjectType({
+  name: "Company",
+  fields: {
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString },
+  },
+});
 
 const UserType = new GraphQLObjectType({
   name: "User",
@@ -13,6 +17,14 @@ const UserType = new GraphQLObjectType({
     id: { type: GraphQLString },
     firstName: { type: GraphQLString },
     age: { type: GraphQLInt },
+    company: {
+      type: CompanyType,
+      resolve(parentValue, args) {
+        return axios
+          .get(`http://localhost:3000/companies/${parentValue.companyId}`)
+          .then((resp) => resp.data);
+      },
+    },
   },
 });
 
@@ -23,7 +35,9 @@ const RootQuery = new GraphQLObjectType({
       type: UserType,
       args: { id: { type: GraphQLString } },
       resolve(parentValue, args) {
-        return _.find(users, { id: args.id });
+        return axios
+          .get(`http://localhost:3000/users/${args.id}`)
+          .then((resp) => resp.data);
       },
     },
   },
